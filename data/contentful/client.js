ff3ed65cfee5adc54879abf59f35a288e94d398a
Jonathan Dierckens <jonathan.dierckens@rombit.be>
2017-08-07 19:22:55 +0200

    require only when needed

--- data/contentful/client.js
+++ data/contentful/client.js
@@ -1,10 +1,12 @@
-const config = require("./config.json");
 const contentful = require("contentful");
 
 module.exports = contentful.createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
-  space: process.env.CONTENTFUL_SPACE_ID || config.CONTENTFUL_SPACE_ID,
+  space:
+    process.env.CONTENTFUL_SPACE_ID ||
+    require("./config.json").CONTENTFUL_SPACE_ID,
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
   accessToken:
-    process.env.CONTENTFUL_ACCESS_TOKEN || config.CONTENTFUL_ACCESS_TOKEN
+    process.env.CONTENTFUL_ACCESS_TOKEN ||
+    require("./config.json").CONTENTFUL_ACCESS_TOKEN
 });
