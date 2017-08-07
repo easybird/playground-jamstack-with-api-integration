c67e1a14385973cd00fa8e6e19f0b29e7c2bfb8e
Jonathan Dierckens <jonathan.dierckens@rombit.be>
2017-08-07 19:15:47 +0200

    add process.env

--- data/contentful/client.js
+++ data/contentful/client.js
@@ -3,7 +3,8 @@ const contentful = require("contentful");
 
 module.exports = contentful.createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
-  space: config.SPACE_ID,
+  space: process.env.CONTENTFUL_SPACE_ID || config.CONTENTFUL_SPACE_ID,
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: config.ACCESS_TOKEN
+  accessToken:
+    process.env.CONTENTFUL_ACCESS_TOKEN || config.CONTENTFUL_ACCESS_TOKEN
 });
